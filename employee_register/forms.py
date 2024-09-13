6c3c13b34f8e66f438b1eee89df0ba332c0cb13e
GLskill <Glebluz1488@gmail.com>
2024-09-13 15:31:02 +0200

    adding a button "Submit"

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -12,7 +12,8 @@ class EmployeeForm(forms.ModelForm):
             'emp_code': 'EMP.Code'
         }
 
-
-
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_ladel = "Select"
 
 
